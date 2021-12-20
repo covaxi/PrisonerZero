077fc38d8eb3c8865997d6f80efc77184d4b1e29
Ivan Chernogorsky <covaxi@gmail.com>
2021-12-20 23:31:55 +0300

    Configuration added

--- PrisonerZero/Configuration.cs
+++ PrisonerZero/Configuration.cs
@@ -8,10 +8,10 @@ namespace PrisonerZero
 {
     internal class Configuration
     {
-        public const string BotToken = "123123";
+        public const string BotToken = "2009530449:AAGCv55sdZvx2Je5sM_zckohyy-AH72uKlA"; 
         public const string BotNick = "prisoner_zero_bot";
-        public const string GoogleApiKey = "123123";
-        // public const string GoogleSearchId = "123123";
-        public const string GoogleSearchId = "123123";
+        public const string GoogleApiKey = "AIzaSyARIJc2A2T2HlP2MXvAi4ZnMfcH4cNZL50";
+        // public const string GoogleSearchId = "106824955141338751498";
+        public const string GoogleSearchId = "c6b9f924148d74e79";
     }
 }
