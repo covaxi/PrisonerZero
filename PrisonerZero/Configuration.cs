aab44401504f6d9163c2d9c6237b85a239eb6dfc
Ivan Chernogorsky <covaxi@gmail.com>
2023-12-06 17:46:06 +0200

    Panic!

---
+++ PrisonerZero/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrisonerZero
+{
+    internal class Configuration
+    {
+        public const string BotToken = "1234"; 
+        public const string BotNick = "prisoner_zero_bot";
+        public const string GoogleApiKey = "4567";
+        // public const string GoogleSearchId = "106824955141338751498";
+        public const string GoogleSearchId = "890";
+    }
+}
